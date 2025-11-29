ce29c21babe650924429035bb3035e163baa32ae
Daniel Okoronkwo <danielokoronkwo90@gmail.com>
2025-11-30 00:01:39 +0100

    Implement a REPL

--- app/main.go
+++ app/main.go
@@ -8,11 +8,13 @@ import (
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error reading input: %s\n", err)
-		os.Exit(1)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error reading input: %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
