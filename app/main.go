910e99ef7bcde73839406dd6a75a09d0b162cd25
Daniel Okoronkwo <danielokoronkwo90@gmail.com>
2025-11-30 01:43:15 +0100

    Locate executable files

--- app/main.go
+++ app/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"slices"
 	"strings"
 )
 
 var builtins = []string{"echo", "exit", "type"}
+var StopWalk = errors.New("command found, stopping walk")
 
 func main() {
 	for {
@@ -37,8 +41,48 @@ func main() {
 
 func handleTypeCommand(command string) {
 	if !slices.Contains(builtins, command) {
+		fullPath := findExecutableInPath(command)
+		if fullPath != "" {
+			fmt.Println(command + " is " + fullPath)
+			return
+		}
 		fmt.Println(command + ": not found")
 	} else {
 		fmt.Println(command + " is a shell builtin")
 	}
+}
+
+func findExecutableInPath(command string) string {
+	envPath := os.Getenv("PATH")
+	dirs := strings.Split(envPath, ":")
+	var cmdPath string
+PATH_LOOP:
+	for _, dir := range dirs {
+		filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+			if !d.IsDir() && d.Name() == command {
+				info, err := os.Stat(path)
+				if err != nil {
+					fmt.Printf("Warning: Failed to get info for %s: %v\n", path, err)
+					return nil
+				}
+
+				mode := info.Mode()
+
+				if mode&0111 != 0 {
+					cmdPath = path
+					return StopWalk
+				}
+			}
+
+			return nil
+		})
+
+		if cmdPath != "" {
+			break PATH_LOOP
+		}
+	}
+	return cmdPath
 }
